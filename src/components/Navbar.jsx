618ed53a15993b87ada3faf83996345b18b7827b
Frecuero <96363833+Frecuero@users.noreply.github.com>
2022-11-18 19:56:51 -0500

    navbar
    Colores

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,9 +13,15 @@ const Navbar = (props) => {
 
   return (
     <div className="container">
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#"> </a>
+        <nav className="navbar " >
+            <div className="container-fluid mt-2">
+                <a className="navbar-brand" href="/">
+                    <img src="https://www.cuc.edu.co/wp-content/uploads/2021/07/logo1cuc.png" height="60px" width='180px' alt="logo cuc" />
+                </a>
+                <div className='d-flex'>
+                    <Link className='btn btn-primary m-2' to='/login'>Login</Link>
+                    <Link className='btn btn-primary m-2' to='/inicio'>Inicio</Link>
+                </div>
             </div>
         </nav>
     </div>
