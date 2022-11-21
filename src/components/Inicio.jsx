8413e6e8c8f9192b9dbef483d86f1c4a81b7d41f
Frecuero <96363833+Frecuero@users.noreply.github.com>
2022-11-21 12:45:18 -0500

    start - finished
    Se termino la ventana de inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -9,17 +9,43 @@ const Inicio = () => {
                     <h1 className='align-items-center mr-5'>Nuestra historia</h1>
                     <div className='border-bottom border-warning border-5 me-5 mb-5'></div>
                 </div>
-                <div className='text-center'>
-                    <img src="https://sfingenieria.com.co/wp-content/uploads/2019/10/foto_bloq_11.png" 
-                        width='300px' height='200px' alt="bloque 11" />
+                <div className='ms-2'>
+                    <img className='rounded' 
+                    src="https://sfingenieria.com.co/wp-content/uploads/2019/10/foto_bloq_11.png" 
+                        width='350px' height='225px' alt="bloque 11" />
                 </div>
             </div>
-            <div className='col bg-primary col-sm-12 col-lg-4'>
-                col 2
-                https://www.cuc.edu.co/la-universidadc
+            <div className='col bg-warning bg-opacity-75 rounded-start col-sm-12 col-lg-4 mt-5'>
+                <div className='row m-2'>
+                    Con el propósito de contribuir al desarrollo educativo regional a nivel superior, se creó el 16 de Noviembre de 1970, la UNIVERSIDAD DE LA COSTA –CUC, entidad sin ánimo de lucro, dedicada a la formación de profesionales en el área de la ciencia, la tecnología, las humanidades, el arte y la filosofía.
+                </div>
+                <div className='row m-2'>
+                    El 3 de enero de 1971, el nuevo centro inició labores en la carrera 42F No. 75B-169 de esta ciudad, ofreciendo los programas de Arquitectura, Administración, Derecho e Ingeniería Civil, con una matrícula de 154 estudiantes. El traslado a su sede actual, Calle 58 No. 55-66, se verificó en enero de 1974.
+                    Su personería jurídica fue otorgada el 23 de abril de 1971, mediante la Resolución No. 352 de la Gobernación del Atlántico.
+                </div>
+                <div className='row m-2'>
+                    Posteriormente, se iniciaron estudios en la Facultad de Ciencias de la Educación, en las especialidades de Psicopedagogía, Matemáticas, Lenguas Modernas y Educación Física, así como en Economía en Comercio Internacional.
+                </div>
+                <div className='row m-2'>
+                    En 1975 se crea el Departamento de Investigaciones Socioeconómicas (DIS) adscrito a la facultad de Economía, hecho que marcó el inicio del proceso investigativo en la CUC.
+                </div>
+                <div className='row m-2'>
+                    En la Década de los 90 ́ el ICFES autoriza el funcionamiento de los programas de INGENIERIA ELECTRICA, INGENIERIA ELECTRONICA, INDUSTRIAL, SANITARIA Y AMBIENTAL, ANÁLISIS Y PROGRAMACIÓN DE COMPUTADORES, TECNOLOGÍA EN INFORMÁTICA Y TELECOMUNICACIONES, luego el programa de PSICOLOGÍA
+                </div>
             </div>
-            <div className='col bg-warning col-sm-12 col-lg-4'>
-                col 2
+            <div className='col bg-warning bg-opacity-75 rounded-end col-sm-12 col-lg-4 ps-3 mt-5'>
+                <div className='row m-2'>
+                    A mediados de 1994 se realizó un diagnostico con el fin de determinar la eficacia y la eficiencia que tenia la gestión investigativa en la institución, a raíz de las inquietudes generadas por la aprobación de la Ley 30/92. El Consejo Directivo determina la implementación y puesta en marcha del Centro de Investigaciones y Desarrollo -CID- que se pone en marcha en 1996, ente interdisciplinario, al cual se adscriben los investigadores pertenecientes a los anteriores centros de las facultades.
+                </div>
+                <div className='row m-2'>
+                    Los programas de Postgrado se les dan inició el 16 de marzo de 1987 con la Especialización en Finanzas y Sistemas, autorizado mediante el Acuerdo 203 del 30 de octubre de 1986, emanado de la Junta Directiva del ICFES.
+                </div>
+                <div className='row m-2'>
+                    Luego, se decide a mediados del año 2000 crear la División de Consultoría y de prestación de servicios adscrito al -CID-, teniendo en cuenta su entorno y las fortalezas que la institución posee o genere durante los próximos años, con el propósito de que la institución mantenga su función de cambio social, económico y ambiental de la Región Caribe.
+                </div>
+                <div className='row m-2'>
+                La UNIVERSIDAD DE LA COSTA ha respondido a la confianza de la comunidad regional formando más de 20000 profesionales en las distintas áreas del conocimiento.
+                </div>
             </div>
 
         </div>
